38695c0adc099cc13d4c01a8062fb00a3a8d3981
Kratika444 <kratikanigam25@gmail.com>
2025-09-20 09:42:05 +0000

    conversion of datatypes

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -36,3 +36,58 @@ console.log(booleanIsLoggedIn)
 let num =33
 let stringNum= String(num)
 console.log(stringNum)
+
+
+// *************** Operations ******************//
+let value=3
+let negvalue= - value 
+console.log(negvalue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**3)
+console.log(2%3)
+
+let str1= "kratika"
+let str2= " nigam"
+let str3= str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(true)     // true 
+
+console.log(+true)    // 1
+console.log(+"")      // 0
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter)
+gameCounter++
+console.log(gameCounter)
+
+// example ->
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+let aa= 2;
+let bb=5;
+console.log(`aa:${aa},bb:${bb}`);
+
