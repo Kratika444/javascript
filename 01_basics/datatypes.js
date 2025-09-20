f119cce37c0bc7e7896192035dd9625c831487d5
Kratika444 <kratikanigam25@gmail.com>
2025-09-20 09:14:06 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use script"; // treat all js code as newer version 
+// javascript ka engine broswer me chupa hua h ..
+
+
+console.log("kratika")
+
+let name = "kratika "
+let age =18       // number datatype 
+let isloggedin = false 
+let state ;  // value not defined 
+let st= null   //  st has empty value 
+
+// datatypes in js- 
+// primitive -
+// number -> range -> 2^53
+// bigint 
+// string= " "
+// boolean-> true / false 
+// null -> standalone value 
+// undefined 
+// symbol -> to find uniqueness 
+
+// object 
+
+console.log(typeof age );
+console.log(typeof null);
+console.log(typeof undefined);
